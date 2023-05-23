d2f4abcbeb476d8afc1d3145699641cfca5d84de
krish-na-br <krishnabrsivam@gmail.com>
2023-05-23 16:52:30 +0530

    first commit

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,24 +1,38 @@
 import React, { useState } from 'react';
 
+
+
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+
 
   const increment = () => {
     setCount(count + 1);
   };
 
+
+
   const decrement = () => {
     setCount(count - 1);
   };
 
+
+
   return (
     <div>
-      <h1>Counter App</h1>
+      <h1>Counter</h1>
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      {count > 0 && <button onClick={decrement}>Decrement</button>}
     </div>
   );
 };
 
+
+
 export default Counter;
+
+
+
+
